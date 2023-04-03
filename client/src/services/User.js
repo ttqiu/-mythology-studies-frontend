b372c082cb41af0eb9ee4f32daecb57b6667e42c
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-04-03 00:32:16 -0400

    create services/User.js

---
+++ client/src/services/User.js
@@ -0,0 +1,29 @@
+import Client from './api'
+
+export const SignInUser = async (data) => {
+  try {
+    const res = await Client.post('/api/users/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/api/users/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/api/users/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
