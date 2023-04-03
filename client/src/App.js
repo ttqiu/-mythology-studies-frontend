9d52490e20a7ae016076b27e1d7bcb9065addd22
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-04-03 01:11:40 -0400

    fixed app.js, index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import Client from './services/api'
+import { CheckSession } from './services/User'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import Account from './pages/Account'
+// import AccountSetting from './pages/AccountSetting'
+// import ClassEnrollment from './pages/ClassEnrollment'
+// import Lobby from './pages/Lobby'
+// import ClassPreview from './pages/ClassPreview'
+// import ClassDetails from './pages/ClassDetails'
+import About from './pages/About'
+import CommentForm from './components/CommentForm'
+import UpdateComment from './components/UpdateComment'
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const [account, setAccount] = useState({})
+
+  useEffect(() => {
+    if (user) {
+      const handleAccount = async () => {
+        const data = await Client.get(`/api/users/info/${user.id}`)
+        setAccount(data.data)
+      }
+      handleAccount()
+    }
+  }, [user, setAccount])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signIn" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/account/"
+            element={<Account user={user} account={account} />}
+          />
+          {/* <Route
+            path="/account/setting"
+            element={<AccountSetting user={user} account={account} />}
+          />
+          <Route path="/lobby" element={<Lobby user={user} />} />
+          <Route
+            path="/creaturePreview/:name"
+            element={<CreaturePreview user={user} />}
+          /> */}
+          <Route
+            path="/form/:userId/:creatureId"
+            element={<CommentForm user={user} />}
+          />
+          <Route
+            path="/updateForm/:creatureId/:id"
+            element={<UpdateComment user={user} />}
+          />
+          {/* <Route
+            path="/creatureDetails/:id"
+            element={<CreatureDetails user={user} account={account} />}
+          /> */}
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Account.js
@@ -0,0 +1,50 @@
+import { useNavigate } from 'react-router-dom'
+
+const Account = ({ user, account }) => {
+  let navigate = useNavigate()
+
+  return user ? (
+    <div className="account-container">
+      <ul style={{ listStyle: 'none' }} className="dashboard">
+        <li>
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-256/account-269-866236.png"
+            alt="info"
+          ></img>
+          <label className="labels" style={{ fontWeight: 'bolder' }}>
+            Account Information
+          </label>
+        </li>
+        <li onClick={() => navigate('/account/setting')}>
+          <img
+            src="https://static.thenounproject.com/png/1110062-200.png"
+            alt="update"
+          ></img>
+          <label className="labels" style={{ fontWeight: 'bolder' }}>
+            Account Setting
+          </label>
+        </li>
+        <li onClick={() => navigate('/account/enrollment')}>
+          <img
+            src="https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1665434595/janesvillek12wius/qquo71eei8xreklnwgpb/EnrollmentIcon.png"
+            alt="enroll"
+          ></img>
+          <label className="labels" style={{ fontWeight: 'bolder' }}>
+            Class Enrollment
+          </label>
+        </li>
+      </ul>
+      <div className="panel" style={{ fontWeight: 'bolder' }}>
+        <h3>userName: {account.userName}</h3>
+        <h3>Email: {account.email}</h3>
+      </div>
+    </div>
+  ) : (
+    <div className="protected">
+      <h3>Oops! You must be signed in to do that!</h3>
+      <button onClick={() => navigate('/signin')}>Sign In</button>
+    </div>
+  )
+}
+
+export default Account
